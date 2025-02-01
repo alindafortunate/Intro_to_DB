c4c094265de6f1e13ceb530e5f91101eb60d6760
alindafortunate <alindafortunate5@gmail.com>
2025-02-01 18:28:02 +0300

    Changed Order Details

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS Order_Details(orderdetailid INT PRIMARY KEY,
 order_id INT, 
 FOREIGN KEY (order_id) REFERENCES Orders(order_id),
 book_id INT,
-FOREIGN KEY(book_id) REFERENCES Books(book_id),
-quantity FLOAT
+quantity FLOAT,
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 )
 
