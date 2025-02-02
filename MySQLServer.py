a853e37a93f20577103fff168d711dbdfb611662
alindafortunate <alindafortunate5@gmail.com>
2025-02-02 15:52:22 +0300

    Complete commit for task1

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,10 +3,9 @@ import mysql.connector
 
 try:
     mydb = mysql.connector.connect(
-        host="localhost",
-        user="alindafortunate",
+        host=config("host"),
+        user=config("user"),
         password=config("password"),
-        database="alx_book_store",
         auth_plugin="mysql_native_password",
     )
     mycursor = mydb.cursor()
